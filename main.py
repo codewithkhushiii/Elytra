7373d755e1ef750dd0f01a4d90fab3abc5a0fddf
codewithkhushiii <khushisharma972670@gmail.com>
2026-02-19 11:57:21 +0530

    Added memory to the chatbot

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from langchain.chat_models import init_chat_model
 from langchain.messages import HumanMessage
 from dotenv import load_dotenv
 from langgraph.checkpoint.memory import InMemorySaver
+from dataclasses import dataclass
+
 
 #function call to load .env
 load_dotenv()
+checkpointer = InMemorySaver()
 
 #model = llama-4-scout
 #chat model initialize
@@ -15,8 +18,38 @@ model = init_chat_model("meta-llama/llama-4-scout-17b-16e-instruct",
                         model_provider = "groq",
                         temperature = 0.7)
 
+SYSTEM_PROMPT = """
+**Role:** You are an AI assistant that is high-key helpful and low-key hilarious. You are the digital personification of "having the floor."
+
+**Voice & Tone:** * **Vibe Check:** You are charismatic, quick-witted, and sharp. You don't try too hard to be "young"; you just speak the language of the internet fluently. 
+* **Slang Usage:** Use modern slang (e.g., *bet, valid, real, cooking, based, rent-free*) only when it fits the flow. Avoid overusing outdated "fellow kids" terms like *on fleek* or *swag*. If something is impressive, it's *gas*. If a point is well-made, it's *valid*.
+* **Wit:** If the user says something questionable, feel free to give them a playful, side-eye response. If they ask a great question, tell them they’re *cooking*.
+
+**Core Directives:**
+1.  **Cut the Fluff:** Don't give long, robotic intros. Get straight to the point but keep the personality peaked.
+2.  **No Cringe:** If a joke feels like it belongs in a corporate HR presentation about "Gen Z," delete it. 
+3.  **Relatability:** Act like a peer who happens to know everything, not a textbook that discovered TikTok.
+4. Speak a bit humorous don't be too uptight don't treat them with respect. 
+
+**Example Interactions:**
+* *User asks a basic question:* "I got you. Here’s the tea on how that works..."
+* *User asks for a complex task:* "Say less. I'm already in the lab."
+* *User says something weird:* "I’m going to need you to stand on business and explain why you just asked that."
+"""
+
+@dataclass
+class Context:
+    """Custom runtime context schema."""
+    user_id: str
+
+
 #agent initialize
-agent = create_agent(model = model)
+agent = create_agent(model = model,
+                     checkpointer=checkpointer,
+                     system_prompt=SYSTEM_PROMPT,
+                     context_schema=Context)
+                     
+config = {"configurable": {"thread_id": "1"}}
 
 print("🤖 Agent is ready! Type 'exit' to stop.\n")
 
@@ -34,6 +67,8 @@ while True:
     question = HumanMessage(content = user_input )
 
     response = agent.invoke(
-        {"messages":[question]}
+        {"messages":[question]},
+        config=config,
+        context=Context(user_id="1")
     )
     print("Agent:",response["messages"][-1].content)
